7374e1f8b5fd1d0fbe0898c0d2dad08b9f4e6161
Totheskysx10 <crazycat87654@gmail.com>
2024-10-25 23:16:21 +0500

    Описание основных сущностей для первого задания

---
+++ src/main/java/naumenproject/naumenproject/configuration/BotService.java
@@ -0,0 +1,45 @@
+package naumenproject.naumenproject.configuration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Service
+public class BotService extends TelegramLongPollingBot {
+
+    private static final Logger log = LoggerFactory.getLogger(BotService.class);
+
+    @Value("${bot.name}")
+    private String botName;
+    @Value("${bot.token}")
+    private String botToken;
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            SendMessage message = SendMessage
+                    .builder()
+                    .chatId(chatId)
+                    .text(messageText)
+                    .build();
+            log.info(message.getText());
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botName;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+}
